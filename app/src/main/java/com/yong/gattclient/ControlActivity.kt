b47d2c760064bdabce5f15fb1f590954f123f84b
yymin1022 <yymin1022@gmail.com>
2024-08-06 11:43:12 +0900

    Update ControlActivity : Added BLE Service Binding

--- app/src/main/java/com/yong/gattclient/ControlActivity.kt
+++ app/src/main/java/com/yong/gattclient/ControlActivity.kt
@@ -1,12 +1,38 @@
 package com.yong.gattclient
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ControlActivity : AppCompatActivity() {
+    private var bleCommandService: BleCommandService? = null
+    private var isServiceBinded = false
+
+    private var bleDeviceAddress: String? = null
+
+    private val bleCommandServiceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val serviceBinder = service as BleCommandService.LocalBinder
+            bleCommandService = serviceBinder.getService()
+            isServiceBinded = true
+            bleDeviceAddress?.let {
+                bleCommandService?.connectBle(it)
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            bleCommandService = null
+            isServiceBinded = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +41,20 @@ class ControlActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        bleDeviceAddress = intent.getStringExtra("BLE_DEVICE_ADDRESS")
+        val serviceIntent = Intent(applicationContext, BleCommandService::class.java)
+        startService(serviceIntent)
+        bindService(serviceIntent, bleCommandServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(isServiceBinded) {
+            unbindService(bleCommandServiceConnection)
+            isServiceBinded = false
         }
     }
 }
