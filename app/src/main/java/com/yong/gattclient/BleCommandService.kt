cedc3bfc052cf4f209a8853ddd67c139121078f1
yymin1022 <yymin1022@gmail.com>
2024-08-06 11:23:55 +0900

    Update BleCommandService : Added Binding Logic

--- app/src/main/java/com/yong/gattclient/BleCommandService.kt
+++ app/src/main/java/com/yong/gattclient/BleCommandService.kt
@@ -2,10 +2,21 @@ package com.yong.gattclient
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
 class BleCommandService: Service() {
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    private val binder = LocalBinder()
+
+    inner class LocalBinder: Binder() {
+        fun getService(): BleCommandService = this@BleCommandService
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        return binder
     }
 }
