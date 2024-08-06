f41e61b5327d030033742223dfcf51de2b318706
yymin1022 <yymin1022@gmail.com>
2024-08-06 10:49:29 +0900

    Update MainActivity : Added Button Listening

--- app/src/main/java/com/yong/gattclient/MainActivity.kt
+++ app/src/main/java/com/yong/gattclient/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.yong.gattclient
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private var btnStartScan: Button? = null
+    private var btnStopScan: Button? = null
+    private var recyclerBleResult: RecyclerView? = null
+
+    private var isBleScanning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnStartScan = findViewById(R.id.main_btn_start_scan)
+        btnStopScan = findViewById(R.id.main_btn_stop_scan)
+        recyclerBleResult = findViewById(R.id.main_recycler_ble_list)
+
+        btnStartScan!!.setOnClickListener(btnListener)
+        btnStopScan!!.setOnClickListener(btnListener)
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.main_btn_start_scan -> startBleScan()
+            R.id.main_btn_stop_scan -> stopBleScan()
+        }
+    }
+
+    private fun startBleScan() {
+        if(!isBleScanning) {
+            isBleScanning = true
+        }
+    }
+
+    private fun stopBleScan() {
+        if(isBleScanning) {
+            isBleScanning = false
         }
     }
 }
